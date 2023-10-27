c05c8d22fc2a6e5e21569560e54a1a0828fe4825
marton.racz <martonracz@apexlab.io>
2023-10-27 16:48:35 +0200

    fix threshold

--- Player.js
+++ Player.js
@@ -1,28 +1,27 @@
 const GameState = require("./src/GameState");
-const lowThreshold = 18;
-const midThreshold = 16;
+const lowThreshold = 16;
+const midThreshold = 15;
 const PAIR_SCORE = 1;
 
 const matcher = (card1, card2) => {
   if (card1.rank() + 1 === card2.rank() || card2.rank() + 1 === card1.rank()) {
     return PAIR_SCORE;
-  } 
-}
+  }
+};
 
 const hasPair = (cards) => {
-  return cards[0].rank() === cards[1].rank()
-}
+  return cards[0].rank() === cards[1].rank();
+};
 class Player {
   static get VERSION() {
     return "0.1";
-  }  
-
+  }
 
   static calculateScore(commonCards, selfCards) {
     let val = 0;
     for (const card1 of commonCards) {
       for (const card2 of selfCards) {
-        val += matcher(card1, card2)
+        val += matcher(card1, card2);
       }
     }
     return val;
@@ -30,19 +29,18 @@ class Player {
 
   static betRequest(gameState, bet) {
     var game = new GameState(gameState);
-    const commonCards = game.communityCards()
-    const selfCards = game.me().holeCards()
-    const baseScore = this.calculateScore(commonCards, selfCards)
-    const adjustedScore = game.me().score() + 0// baseScore
-    if (adjustedScore > lowThreshold || hasPair(game.me().holeCards())) {
-     bet(game.toRaiseByBlinds(50));
-    } else if (adjustedScore > midThreshold ) {
+    const commonCards = game.communityCards();
+    const selfCards = game.me().holeCards();
+    const baseScore = this.calculateScore(commonCards, selfCards);
+    const adjustedScore = game.me().score() + 0; // baseScore
+    if (adjustedScore >= lowThreshold || hasPair(game.me().holeCards())) {
+      bet(game.toRaiseByBlinds(50));
+    } else if (adjustedScore > midThreshold) {
       bet(game.toCall());
     }
-    bet(0)
+    bet(0);
   }
-  static showdown(gameState) {
-  }
+  static showdown(gameState) {}
 }
 
 module.exports = Player;
