0e7751d5c509760f7bd0af1b22950a51dd03dfaf
marton.racz <martonracz@apexlab.io>
2023-10-27 15:08:50 +0200

    feat: thresholds

--- Player.js
+++ Player.js
@@ -1,18 +1,21 @@
-const GameState = require('./src/GameState');
-
+const GameState = require("./src/GameState");
+const lowThreshold = 14;
+const midThreshold = 10;
 class Player {
   static get VERSION() {
-    return '0.1';
+    return "0.1";
   }
 
   static betRequest(gameState, bet) {
     var game = new GameState(gameState);
-    bet(0);
+    if (game.me().score() > lowThreshold) {
+      game.toRaise();
+    } else if (game.me().score() > midThreshold) {
+      game.toCall();
+    }
+    bet()
   }
-
-  static showdown(gameState) {
-  }
+  static showdown(gameState) {}
 }
 
 module.exports = Player;
-
